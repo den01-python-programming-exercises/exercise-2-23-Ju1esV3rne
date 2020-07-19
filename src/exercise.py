59b67dcd22e23205abfc09b84e91bacaaddf248f
Vladimir Slysar <g7au2ss@gmail.com>
2020-07-20 03:04:19 +0700

    2

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
+class ClassName(object):
+    """docstring for ."""
+
+    def __init__(self):
+        self.print_until_number(2)
+
+    def print_until_number(self, number):
+        i = 0
+        while i < number:
+            print(i + 1)
+            i += 1
+
+
 def main():
     #write your code below this line
+    ClassName()
 
 if __name__ == '__main__':
     main()
